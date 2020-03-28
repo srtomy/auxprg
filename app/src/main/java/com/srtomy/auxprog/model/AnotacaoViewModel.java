a3555f7b20940f05d71ec21ee75d8f7a2cb76a9a
SrTomy <tarcisioscotta2@gmail.com>
2020-03-28 13:49:42 -0300

    anotacao: add model

---
+++ app/src/main/java/com/srtomy/auxprog/model/AnotacaoViewModel.java
@@ -0,0 +1,33 @@
+package com.srtomy.auxprog.model;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.srtomy.auxprog.Anotacao;
+import com.srtomy.auxprog.repository.AnotacaoRepository;
+
+import java.util.List;
+
+public class AnotacaoViewModel extends AndroidViewModel {
+
+    private AnotacaoRepository repo;
+    private LiveData<List<Anotacao>> anotacoes;
+
+    public AnotacaoViewModel(@NonNull Application application) {
+        super(application);
+
+        repo = new AnotacaoRepository(application);
+        anotacoes = repo.getAnotacoes();
+    }
+
+    public LiveData<List<Anotacao>> getAllWords() {
+        return anotacoes;
+    }
+
+    public void insert(Anotacao anotacao) {
+        repo.insere(anotacao);
+    }
+}
