e28437f0255e21b66557dcf506251c283693f9b8
SrTomy <tarcisioscotta2@gmail.com>
2020-03-29 17:03:00 -0300

    anotacao details: update view e backend

--- app/src/main/java/com/srtomy/auxprog/activity/AnotacaoDetailsActivity.java
+++ app/src/main/java/com/srtomy/auxprog/activity/AnotacaoDetailsActivity.java
@@ -5,51 +5,80 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.srtomy.auxprog.R;
 import com.srtomy.auxprog.Anotacao;
+import com.srtomy.auxprog.R;
+import com.srtomy.auxprog.activity.utils.Validator;
 import com.srtomy.auxprog.model.AnotacaoViewModel;
 
+import java.time.LocalDateTime;
+
 public class AnotacaoDetailsActivity extends AppCompatActivity {
-    private AnotacaoViewModel mWordViewModel;
+    private Anotacao anotacao;
 
-    public static final String EXTRA_REPLY = "com.example.android.wordlistsql.REPLY";
-
-    private EditText mEditWordView;
     private EditText txtTitulo;
     private EditText txtDescricao;
     private EditText txtCategoria;
+    private Button btnSalvar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anotacao_details);
-        mEditWordView = findViewById(R.id.txtTitulo);
 
-        // Get a new or existing ViewModel from the ViewModelProvider.
-        mWordViewModel = new ViewModelProvider(this).get(AnotacaoViewModel.class);
+        initLayout();
 
-        final Button button = findViewById(R.id.button_save);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent replyIntent = new Intent();
-                if (TextUtils.isEmpty(mEditWordView.getText())) {
-                    setResult(RESULT_CANCELED, replyIntent);
-                } else {
-                    Anotacao anotacao = new Anotacao(mEditWordView.getText().toString());
-                    replyIntent.putExtra("anotacao",anotacao);
-                    setResult(RESULT_OK, replyIntent);
-                }
-                finish();
-            }
-        });
+        initActions();
+
+        anotacao = (Anotacao) getIntent().getExtras().get("anotacao");
+        setAnotacao(anotacao);
     }
 
     private void initLayout(){
+        txtTitulo = findViewById(R.id.txtTitulo);
+        txtCategoria = findViewById(R.id.txtCategoria);
+        txtDescricao = findViewById(R.id.txtDescricao);
 
+        btnSalvar =findViewById(R.id.button_save);
+    }
+
+    private void initActions(){
+
+        btnSalvar.setOnClickListener(evt->{
+            Intent replyIntent = new Intent();
+
+            if (validar()) {
+                anotacao.setTitulo(txtTitulo.getText().toString());
+                anotacao.setCategoria(txtCategoria.getText().toString());
+                anotacao.setDescricao(txtDescricao.getText().toString());
+
+                replyIntent.putExtra("anotacao",anotacao);
+                setResult(RESULT_OK, replyIntent);
+
+                finish();
+            } else {
+                 //setResult(RESULT_CANCELED, replyIntent);
+            }
+
+        });
+    }
+
+    private boolean validar(){
+
+        boolean tituloValid = Validator.validateNotNull(txtTitulo,"Titulo invalido");
+        boolean categoriaValid = Validator.validateNotNull(txtCategoria,"Categoria invalida");
+        boolean descricaoValid = Validator.validateNotNull(txtDescricao,"Descrição invalida");
+
+        return tituloValid && categoriaValid && descricaoValid;
+    }
+
+    private void setAnotacao(Anotacao an){
+        anotacao = an;
+
+        txtDescricao.setText(anotacao.getDescricao());
+        txtTitulo.setText(anotacao.getTitulo());
+        txtCategoria.setText(anotacao.getCategoria());
     }
 }
