14e73f0639547b10a4bfabe66524c3a5f6e30d76
SrTomy <tarcisioscotta2@gmail.com>
2020-05-13 20:55:10 -0300

    add activity main

---
+++ app/src/main/java/com/srtomy/auxprog/activity/MainActivity.java
@@ -0,0 +1,28 @@
+package com.srtomy.auxprog.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.srtomy.auxprog.R;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int ANOTACAO_ACTIVITY_REQUEST_CODE = 1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Button btnAnotacao = findViewById(R.id.btn_anotacao);
+        btnAnotacao.setOnClickListener(evt->{
+            Intent intent = new Intent(this, AnotacaoActivity.class);
+            startActivityForResult(intent, ANOTACAO_ACTIVITY_REQUEST_CODE);
+        });
+    }
+}
