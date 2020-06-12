a16b32675d97c5f0c4b14044452cdd487efc18d4
SrTomy <tarcisioscotta2@gmail.com>
2020-06-11 21:40:42 -0300

    add componente chip anotacao categorias

--- app/src/main/java/com/srtomy/auxprog/activity/AnotacaoDetailsActivity.java
+++ app/src/main/java/com/srtomy/auxprog/activity/AnotacaoDetailsActivity.java
@@ -1,14 +1,20 @@
 package com.srtomy.auxprog.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.core.content.ContextCompat;
 
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.flexbox.FlexboxLayout;
+import com.google.android.material.chip.Chip;
 import com.srtomy.auxprog.Anotacao;
 import com.srtomy.auxprog.R;
 import com.srtomy.auxprog.activity.utils.Validator;
@@ -19,10 +25,9 @@ public class AnotacaoDetailsActivity extends AppCompatActivity {
 
     private EditText txtTitulo;
     private EditText txtDescricao;
-    private EditText txtCategoria;
     private Button btnSalvar;
-    private Button btnDeletar;
-    private Menu _menu = null;
+    private FlexboxLayout layoutCategorias;
+    private EditText txtInputCategoria;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,19 +47,15 @@ public class AnotacaoDetailsActivity extends AppCompatActivity {
     {
         // Inflate the menu_anotacao_details; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_anotacao_details, menu);
-        _menu = menu;
 
         return true;
     }
 
-
-
-
     private void initLayout(){
+        layoutCategorias = findViewById(R.id.recipient_group_FL);
         txtTitulo = findViewById(R.id.txtTitulo);
-        txtCategoria = findViewById(R.id.txtCategoria);
         txtDescricao = findViewById(R.id.txtDescricao);
-
+        txtInputCategoria = findViewById(R.id.recipient_input_ET);
         btnSalvar =findViewById(R.id.button_save);
     }
 
@@ -65,8 +66,18 @@ public class AnotacaoDetailsActivity extends AppCompatActivity {
 
             if (validar()) {
                 anotacao.setTitulo(txtTitulo.getText().toString());
-                anotacao.setCategoria(txtCategoria.getText().toString());
                 anotacao.setDescricao(txtDescricao.getText().toString());
+
+                String categoriasStr = "";
+                for(int i =0; i < layoutCategorias.getChildCount();i++) {
+                    Object view = layoutCategorias.getFlexItemAt(i);
+
+                    if (view instanceof Chip) {
+                        categoriasStr += ((Chip) view).getText() + ";";
+                    }
+                }
+
+                anotacao.setCategoria(categoriasStr);
 
                 replyIntent.putExtra("anotacao",anotacao);
                 setResult(RESULT_OK, replyIntent);
@@ -75,17 +86,28 @@ public class AnotacaoDetailsActivity extends AppCompatActivity {
             } else {
                  //setResult(RESULT_CANCELED, replyIntent);
             }
+        });
 
+        txtInputCategoria.setOnEditorActionListener((v, actionId, dd) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                CharSequence txtVal = v.getText();
+                if (txtVal != null) {
+                    addNewChip(txtVal.toString(), layoutCategorias);
+                    txtInputCategoria.setText("");
+                }
+
+                return true;
+            }
+            return false;
         });
     }
 
     private boolean validar(){
 
         boolean tituloValid = Validator.validateNotNull(txtTitulo,"Titulo invalido");
-        boolean categoriaValid = Validator.validateNotNull(txtCategoria,"Categoria invalida");
         boolean descricaoValid = Validator.validateNotNull(txtDescricao,"Descrição invalida");
 
-        return tituloValid && categoriaValid && descricaoValid;
+        return tituloValid && descricaoValid;
     }
 
     private void setAnotacao(Anotacao an){
@@ -93,6 +115,20 @@ public class AnotacaoDetailsActivity extends AppCompatActivity {
 
         txtDescricao.setText(anotacao.getDescricao());
         txtTitulo.setText(anotacao.getTitulo());
-        txtCategoria.setText(anotacao.getCategoria());
+        for(String str : anotacao.getCategoria().split(";")){
+            addNewChip(str, layoutCategorias);
+        }
+    }
+
+    private void addNewChip(String person, FlexboxLayout chipGroup) {
+        Chip chip = new Chip(this, null, R.style.Widget_MaterialComponents_Chip_Entry);
+        chip.setText(person);
+        chip.setCloseIcon(ContextCompat.getDrawable(this,R.drawable.ic_close_black_24dp));
+        chip.setChipBackgroundColorResource(R.color.colorPrimary);
+        chip.setCloseIconVisible(true);
+        chip.setClickable(true);
+        chip.setCheckable(false);
+        chipGroup.addView(chip, chipGroup.getChildCount() - 1);
+        chip.setOnCloseIconClickListener(evt-> chipGroup.removeView(chip));
     }
 }
