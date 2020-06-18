631308a140250054d7e2c9ac09f4b828070afbdd
SrTomy <tarcisioscotta2@gmail.com>
2020-06-17 22:38:31 -0300

    add material google in issue details

--- app/src/main/java/com/srtomy/auxprog/activity/IssueDetailsActivity.java
+++ app/src/main/java/com/srtomy/auxprog/activity/IssueDetailsActivity.java
@@ -2,10 +2,14 @@ package com.srtomy.auxprog.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import com.google.android.flexbox.FlexboxLayout;
+import com.google.android.material.chip.Chip;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.srtomy.auxprog.Issue;
 import com.srtomy.auxprog.R;
@@ -20,6 +24,7 @@ public class IssueDetailsActivity extends AppCompatActivity {
     private EditText txtDtSolucao;
     private EditText txtDescricao;
     private EditText txtSolucao;
+    private FlexboxLayout layoutCategorias;
     private FloatingActionButton btnSalvar;
 
     @Override
@@ -38,15 +43,37 @@ public class IssueDetailsActivity extends AppCompatActivity {
 
     private void initActions(){
 
+        txtCategoria.setOnEditorActionListener((v, actionId, dd) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                CharSequence txtVal = v.getText();
+                if (txtVal != null) {
+                    addNewChip(txtVal.toString(), layoutCategorias);
+                    txtCategoria.setText("");
+                }
+
+                return true;
+            }
+            return false;
+        });
+
         btnSalvar.setOnClickListener(evt->{
             if(validar()){
                 issue.setTitulo(txtTitulo.getText().toString());
-                issue.setCategorias(txtCategoria.getText().toString());
                 issue.setDtCriacao(txtDtCriacao.getText().toString());
                 issue.setDtSolucao(txtDtSolucao.getText().toString());
                 issue.setDescricao(txtDescricao.getText().toString());
                 issue.setSolucao(txtSolucao.getText().toString());
 
+                String categoriasStr = "";
+                for(int i =0; i < layoutCategorias.getChildCount();i++) {
+                    Object view = layoutCategorias.getFlexItemAt(i);
+
+                    if (view instanceof Chip) {
+                        categoriasStr += ((Chip) view).getText() + ";";
+                    }
+                }
+
+                issue.setCategorias(categoriasStr);
 
                 Intent replyIntent = new Intent();
                 replyIntent.putExtra("issue",issue);
@@ -59,12 +86,13 @@ public class IssueDetailsActivity extends AppCompatActivity {
 
     private void initLayout(){
         txtTitulo = findViewById(R.id.txtTituloIssue);
-        txtCategoria = findViewById(R.id.txtCategoriaIssue);
+        txtCategoria = findViewById(R.id.txtCaregoriaIssue);
         txtDtCriacao = findViewById(R.id.txtDtCriacaoIssue);
         txtDtSolucao = findViewById(R.id.txtDtSolucaoIssue);
         txtDescricao = findViewById(R.id.txtDescricaoIssue);
         txtSolucao = findViewById(R.id.txtSolucaoIssue);
         btnSalvar = findViewById(R.id.btnSalvarIssue);
+        layoutCategorias = findViewById(R.id.categoria_group);
     }
 
     private boolean validar(){
@@ -80,10 +108,29 @@ public class IssueDetailsActivity extends AppCompatActivity {
         this.issue = iss;
 
         txtTitulo.setText(issue.getTitulo());
-        txtCategoria.setText(issue.getCategorias());
         txtDtCriacao.setText(issue.getDtCriacao());
         txtDtSolucao.setText(issue.getDtSolucao());
         txtDescricao.setText(issue.getDescricao());
         txtSolucao.setText(issue.getSolucao());
+
+        if(issue.getCategorias() !=null && !issue.getCategorias().isEmpty()) {
+            for (String str : issue.getCategorias().split(";")) {
+                addNewChip(str, layoutCategorias);
+            }
+        }
     }
+
+    private void addNewChip(String person, FlexboxLayout chipGroup) {
+        Chip chip = new Chip(this, null, R.style.Widget_MaterialComponents_Chip_Entry);
+        chip.setText(person);
+        chip.setCloseIcon(ContextCompat.getDrawable(this,R.drawable.ic_close_black_24dp));
+        chip.setChipBackgroundColorResource(R.color.colorPrimary);
+        chip.setCloseIconVisible(true);
+        chip.setClickable(true);
+        chip.setCheckable(false);
+        chipGroup.addView(chip, chipGroup.getChildCount() - 1);
+        chip.setOnCloseIconClickListener(evt-> chipGroup.removeView(chip));
+
+        FlexboxLayout.LayoutParams lp = (FlexboxLayout.LayoutParams) chip.getLayoutParams();
+        lp.setMargins(5,5,5,5);    }
 }
